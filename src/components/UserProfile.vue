1af98d3363c04c31f3f62491dfbe57db438baba4
André Miller <andremmiller@live.com>
2020-10-27 22:53:10 -0300

    First day twotter

---
+++ src/components/UserProfile.vue
@@ -0,0 +1,151 @@
+<template>
+  <div class="user-profile">
+    <div class="user-profile__user-panel">
+      <h1 class="user-profile__username">@{{ user.username }}</h1>
+      <div class="user-profile__admin-badge" v-if="user.isAdmin">Admin</div>
+      <div class="user-profile__follower-count">
+        <strong>Followers: </strong> {{ followers }}
+      </div>
+      <form class="user-profile__create-twoot" @submit.prevent="createNewTwoot">
+        <label for="newTwoot"><strong>New Twoot</strong></label>
+        <textarea id="newTwoot" rows="4" v-model="newTwootContent"></textarea>
+
+        <div class="user-profile__create-twoot-type">
+          <label for="newTwootType">Type: </label>
+          <select id="newTwootType" v-model="selectedTwootType">
+            <option
+              :value="option.value"
+              v-for="(option, index) in twootTypes"
+              :key="index"
+            >
+              {{ option.name }}
+            </option>
+          </select>
+        </div>
+
+        <button>
+            Twoot!
+        </button>
+
+      </form>
+    </div>
+    <div class="user-profile__twoots-wrapper">
+      <TwootItem
+        v-for="twoot in user.twoots"
+        :key="twoot.id"
+        :username="user.username"
+        :twoot="twoot"
+        @favourite="toggleFavourite"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import TwootItem from "./TwootItem";
+
+export default {
+  name: "UserProfile",
+  components: { TwootItem },
+  data() {
+    return {
+      newTwootContent: "",
+      selectedTwootType: "instant",
+      twootTypes: [
+        { value: "draft", name: "Draft" },
+        { value: "instant", name: "Instant Twoot" },
+      ],
+      followers: 0,
+      user: {
+        id: 1,
+        username: "ammiller",
+        firstName: "André",
+        lastName: "Miller",
+        email: "andremmiller@live.com",
+        isAdmin: true,
+        twoots: [
+          { id: 1, content: "Twooter is amazing!" },
+          { id: 2, content: "Do not forget to subscribe" },
+          { id: 3, content: "Click the button!" },
+        ],
+      },
+    };
+  },
+  watch: {
+    // execute a function when a value changes
+    followers(newFollowerContent, oldFollowerContent) {
+      if (oldFollowerContent < newFollowerContent) {
+        console.log(`${this.user.username} has gained a follower!`);
+      }
+    },
+  },
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName}`;
+    },
+  },
+  methods: {
+    followUser() {
+      this.followers++;
+    },
+    toggleFavourite(id) {
+      console.log(`Favourited twoot: ${id}`);
+    },
+    createNewTwoot() {
+        if (this.newTwootContent && this.selectedTwootType !== 'draft') {
+            this.user.twoots.unshift({ 
+                id: this.user.twoots.length + 1,
+                content: this.newTwootContent
+            })
+
+            this.newTwootContent = ''
+        }
+    }
+  },
+  mounted() {
+    this.followUser();
+  },
+};
+</script>
+
+<style scoped>
+.user-profile {
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  width: 100%;
+  padding: 50px 5%;
+}
+.user-profile__user-panel {
+  display: flex;
+  flex-direction: column;
+  margin-right: 50px;
+  padding: 20px;
+  background-color: white;
+  border-radius: 5px;
+  border: 1px solid #dfe3e8;
+}
+.user-profile__admin-badge {
+  background: red;
+  color: white;
+  border-radius: 5px;
+  margin-right: auto;
+  padding: 0 10px;
+  font-weight: bold;
+}
+
+h1 {
+  margin: 0;
+}
+
+.user-profile__create-twoot {
+  border-top: 1px solid #dfe3e8;
+  border-radius: 5px;
+  margin-right: auto;
+  padding-top: 20px;
+  font-weight: bold;
+}
+
+.user-profile__create-twoot > textarea {
+  width: 100%;
+}
+</style>
